a92fd0f2ffb84176a384565458e897381afd9bec
AnLaxina <anilovlaxina@gmail.com>
2025-12-26 05:10:48 -0800

    feat(detailedMessage): center message details & add button to return to home

--- controllers/detailedMessageController.js
+++ controllers/detailedMessageController.js
@@ -2,9 +2,6 @@ import messages from "../models/messages.js";
 
 export function displayView(req, res) {
     const { userId } = req.params;
-    console.log(
-        `userId is: ${userId}, message.userId is: ${messages[0].userId}`
-    );
     const message = messages.find(
         (message) => message.userId === Number(userId)
     );
