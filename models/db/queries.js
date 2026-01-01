2c62f3270875a4ce1b9e7f0cd6c12a684385c3b9
AnLaxina <anilovlaxina@gmail.com>
2026-01-01 14:14:19 -0800

    feat(queries): add working `create` &  `delete` for messages

--- models/db/queries.js
+++ models/db/queries.js
@@ -2,11 +2,21 @@ import pool from "./pool.js";
 
 export async function getAllMessages() {
     const { rows } = await pool.query("SELECT * FROM messages");
-    // For now, just print them out lol
-    rows.forEach((message) => {
-        console.log(message.username);
-    });
-    await pool.end();
+    return rows;
 }
 
-await getAllMessages();
+export async function createMessage(username, text, added = new Date()) {
+    const query = `INSERT INTO messages (username, text, added) VALUES ($1, $2, $3);`;
+    await pool.query(query, [username, text, added]);
+}
+
+export async function deleteMessage(usernameid) {
+    const query = `DELETE FROM messages
+                WHERE usernameid = $1`;
+
+    await pool.query(query, [usernameid]);
+}
+
+console.log(await getAllMessages());
+// console.log(await createMessage("Chicken", "Doggy"));
+// console.log(await deleteMessage(3));
